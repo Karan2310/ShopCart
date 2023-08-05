1aab5c80e1f7154c5403530a9f1ca24cab92b41e
Karan2310 <karandeep9619@gmail.com>
2023-08-05 22:24:28 +0530

    Unnecessary import removed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Filters from "../components/Filters";
 import ProductGallery from "../components/ProductGallery";
